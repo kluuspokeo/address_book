3f841caa91e6fddf29ef957087797c6448f6f35c
Kevin Luu <kevinluu@macbook104.spokeo.com>
2016-03-28 12:01:51 -0700

    Deleting auto generated helper files and coffee scripts

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -6,70 +6,70 @@ $(document).ready(function(){
 		switch(e.target.className)
 		{
 			case "add":
-				var _template = $("#"+_target.data('template')).html();
-				if (_template) {
-					$(_template).insertBefore(this);
-				}
-				break;
+			var _template = $("#"+_target.data('template')).html();
+			if (_template) {
+				$(_template).insertBefore(this);
+			}
+			break;
 			case "close":
-				_target.parents('div').fadeOut(250, function() {
-					this.remove();
-				});
-				break;
+			_target.parents('div').fadeOut(250, function() {
+				this.remove();
+			});
+			break;
 			case "edit":
-				var _type = _target.parents('.edit_fields').data('type'),
-					_user_id = _target.parents('.edit_fields').data('user-id');
-				switch(_type) {
-					case "address":
-					case "phone_number":
-					case "email_address":
-						var _inputs = $('input, select', _target.parents('.edit_fields')),
-							_params = {};
-						_inputs.each(function(i,el){
-							_params[el.className] = el.value;
-						});
-						$.ajax({
-							"url": "/user/"+_user_id+"/"+_type,
-							"method": "POST",
-							"data": _params,
-							"dataType": "JSON",
-							"success": function(d){
-								if (d.status === "true") { 
-									location.reload();
-								} else {
-									alert(d.message || "Error");
-								}
-							},
-							"error": function(){
-								alert('error occured. try again later');
-							}
-						});
-						break
+			var _type = _target.parents('.edit_fields').data('type'),
+			_user_id = _target.parents('.edit_fields').data('user-id');
+			switch(_type) {
+				case "address":
+				case "phone_number":
+				case "email_address":
+				var _inputs = $('input, select', _target.parents('.edit_fields')),
+				_params = {};
+				_inputs.each(function(i,el){
+					_params[el.className] = el.value;
+				});
+				$.ajax({
+					"url": "/user/"+_user_id+"/"+_type,
+					"method": "POST",
+					"data": _params,
+					"dataType": "JSON",
+					"success": function(d){
+						if (d.status === "true") { 
+							location.reload();
+						} else {
+							alert(d.message || "Error");
+						}
+					},
+					"error": function(){
+						alert('error occured. try again later');
+					}
+				});
+				break
+			}
+			break;
+			case "delete":
+			var _type = _target.data('type'),
+			_user_id = _target.data('user-id'),
+			_id = _target.data('id');
+			switch (_type) {
+				case "phone_number":
+				case "address":
+				case "email_address":
+				if(confirm("Are you sure?")) {
+					$.ajax({
+						"url": "/user/"+_user_id+"/"+_type+"/"+_id,
+						"method": "DELETE",
+						"success": function(d){
+							_target.parents('.'+_type).fadeOut(250);
+						},
+						"error": function(d) {
+							alert('error occured. try again later');
+						}
+					});
 				}
 				break;
-			case "delete":
-				var _type = _target.data('type'),
-					_user_id = _target.data('user-id'),
-					_id = _target.data('id');
-				switch (_type) {
-					case "phone_number":
-					case "address":
-					case "email_address":
-					if(confirm("Are you sure?")) {
-						$.ajax({
-							"url": "/user/"+_user_id+"/"+_type+"/"+_id,
-							"method": "DELETE",
-							"success": function(d){
-								_target.parents('.'+_type).fadeOut(250);
-							},
-							"error": function(d) {
-								alert('error occured. try again later');
-							}
-						});
-					}
-					break;
-				}
-				
+			}
+			
 		}
 		e.preventDefault();
 	});
--- app/helpers/phone_number_helper.rb
+++
@@ -1,2 +0,0 @@
-module PhoneNumberHelper
-end
